3a7eadbc303eac61314e016d185da60f886f1542
azusayoru <71689360+azusayoru@users.noreply.github.com>
2020-10-20 15:41:17 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
+})
+radio.setGroup(1)
+basic.forever(function () {
+    let i = 0
+    if (i % 2 == 0) {
+    	
+    } else {
+    	
+    }
+})
